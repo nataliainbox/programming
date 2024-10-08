bcc69bbb651e6dc515ac91dd2b47d73447a690a5
user <162068602+nataliainbox@users.noreply.github.com>
2024-10-08 07:39:30 -0300

    kk

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 const paragrafo = document.createElement('p')
 paragrafo.classList.add('graficos--container__texto')
 paragrafo.innerHTML = `Está é uma pesquisa do IBGE com o foco em $ {tipo_dados}`
+const paragrafo = document.createElement('')
+paragrafo.classList.add('graficos--container__texto')
+paragrafo.innerHTML = "Aqui será colocado"
+}
+visualizarDados()
