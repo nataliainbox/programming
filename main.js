4eb7d7a897908a26bff336196cee81e7cb43ae8b
user <162068602+nataliainbox@users.noreply.github.com>
2024-10-08 08:00:24 -0300

    6dghn6jyhmj6my59+25595659dhmd9+1r9191m

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-const paragrafo = document.createElement('p')
+
+
+
+
+
+async function visualizarDados(){
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados)
+    regiao = 1 
+resultado_dados = (dados[0]['resultados'][0]["series"][regiao]['serie'])
+resultado_regiao = (dados[0]['resultados'][0]["series"][regiao]["localidade"]['nome'])
+
+console.log(resultado_dados,resultado_regiao)
+
+const pragrafo = document.createElement('p')
 paragrafo.classList.add('graficos--container__texto')
 paragrafo.innerHTML = `Está é uma pesquisa do IBGE com o foco em $ {tipo_dados}`
-const paragrafo = document.createElement('')
-paragrafo.classList.add('graficos--container__texto')
-paragrafo.innerHTML = "Aqui será colocado"
+
+const container = document.getElementById()
+
+container.appedChild(paragrafo)
 }
-visualizarDados()
